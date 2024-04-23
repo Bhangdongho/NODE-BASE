0b29544ba578c5d1cc877dfc386284cf4ee97333
Bhangdongho <bhang97@naver.com>
2024-04-24 08:52:52 +0900

    server setting

--- express-demo.js
+++ express-demo.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.listen(3000);
+
 // GET + "/"
 app.get('/', function (req, res) {
   res.send('Hello World');
@@ -18,4 +20,17 @@ app.get('/test/1', function (req, res) {
   res.send('One!!');
 });
 
-app.listen(3000);
+// /hello, /bye, /nicetomeetyou
+app.get('/hello', function (req, res) {
+  res.send('안녕하세요');
+});
+
+app.get('/bye', function (req, res) {
+  res.send('안녕히 가세요');
+});
+
+// GET 메소드로, '/nicetomeetyou'이 날아오면
+// 매개변수로 전달받은 콜백 함수를 호출한다. => 서버에 셋팅
+app.get('/nicetomeetyou', function (req, res) {
+  res.send('만나서 반갑습니다');
+});
