ca262f0053494830a9d6c2997699ecca107b753e
Bhangdongho <bhang97@naver.com>
2024-04-24 03:41:17 +0900

    is-odd-demo.js

---
+++ is-odd-demo.js
@@ -0,0 +1,10 @@
+const isOdd = require('is-odd-num');
+
+console.log(isOdd('8')); //=> true
+console.log(isOdd('3')); //=> true
+
+console.log(isOdd(7)); //=> false
+console.log(isOdd(2)); //=> false
+
+// npm i is-odd-num
+// 홀수 판단
