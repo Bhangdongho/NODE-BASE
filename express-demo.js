668cf76e0b5f9e38af6e47393edb3f2411a4baa6
Bhangdongho <bhang97@naver.com>
2024-04-24 09:57:26 +0900

    코드 정리, req.params.n

--- express-demo.js
+++ express-demo.js
@@ -9,39 +9,6 @@ app.get('/', function (req, res) {
   res.send('Hello World');
 });
 
-// API : GET + "http://localhost:3000/test"
-// "TEST SUCCESS"
-app.get('/test', function (req, res) {
-  res.send('Test');
-});
-
-// API : GET + "http://localhost:3000/test/1"
-// "One!!"
-app.get('/test/1', function (req, res) {
-  res.send('One!!');
-});
-
-// /hello, /bye, /nicetomeetyou
-app.get('/hello', function (req, res) {
-  res.json({
-    say: '안녕하세요',
-  });
-});
-
-app.get('/bye', function (req, res) {
-  res.json({
-    say: '안녕히 가세요',
-  });
-});
-
-// GET 메소드로, '/nicetomeetyou'이 날아오면
-// 매개변수로 전달받은 콜백 함수를 호출한다. => 서버에 셋팅
-app.get('/nicetomeetyou', function (req, res) {
-  res.json({
-    say: '만나서 반갑습니다',
-  });
-});
-
 let nodejsBook = {
   title: 'Node.js를 공부해보자',
   price: 20000,
---
+++ params-demo.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+// 서버 셋팅 : 포트 넘버(번호) 3000로 셋팅
+app.listen(3000);
+
+app.get('/products/:n', function (req, res) {
+  // : => URL로 매개변수로 전달해주는 거라고 알아 들음
+  // req.params
+  // products/__ 빈칸에 오는 값을 n이라는 변수에 담는다는 뜻
+  //   console.log(req.params);
+  //   console.log(req.params.n);
+
+  res.json({
+    num: req.params.n,
+  });
+});
