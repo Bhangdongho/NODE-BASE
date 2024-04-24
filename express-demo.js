4986f0ff92eedbd8bed1e37479bd6d6783d2cfde
Bhangdongho <bhang97@naver.com>
2024-04-24 09:36:28 +0900

    json 활용

--- express-demo.js
+++ express-demo.js
@@ -23,22 +23,32 @@ app.get('/test/1', function (req, res) {
 
 // /hello, /bye, /nicetomeetyou
 app.get('/hello', function (req, res) {
-  res.send({
+  res.json({
     say: '안녕하세요',
   });
 });
 
 app.get('/bye', function (req, res) {
-  res.send('안녕히 가세요');
+  res.json({
+    say: '안녕히 가세요',
+  });
 });
 
 // GET 메소드로, '/nicetomeetyou'이 날아오면
 // 매개변수로 전달받은 콜백 함수를 호출한다. => 서버에 셋팅
 app.get('/nicetomeetyou', function (req, res) {
-  res.send('만나서 반갑습니다');
+  res.json({
+    say: '만나서 반갑습니다',
+  });
 });
 
+let nodejsBook = {
+  title: 'Node.js를 공부해보자',
+  price: 20000,
+  description: '이 책이 쏼라쏼라~',
+};
+
 app.get('/products/1', function (req, res) {
-  res.send('Node.js를 배워보자 (책)');
+  res.json(nodejsBook);
   // res.send(20000);
 });
