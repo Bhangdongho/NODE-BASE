74129ad6b973d4bca5957e58fd4297830274ac1a
Bhangdongho <bhang97@naver.com>
2024-04-29 06:13:12 +0900

    전체 메세지

--- demo-api/youtuber-demo.js
+++ demo-api/youtuber-demo.js
@@ -83,3 +83,19 @@ app.delete('/youtubers/:id', (req, res) => {
     });
   }
 });
+
+app.delete('/youtubers', (req, res) => {
+  var msg = '';
+  // db에 값이 1개 이상이면, 전체 삭제
+  if (db.size >= 1) {
+    db.clear();
+    msg = '전체 유튜버가 삭제되었습니다.';
+  } else {
+    // 값이 없으면,
+    msg = '삭제할 유튜버가 없습니다.';
+  }
+
+  res.json({
+    message: msg,
+  });
+});
