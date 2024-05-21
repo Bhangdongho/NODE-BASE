faa273e70314f41bd7affebae662698c3b12bd18
Bhangdongho <bhang97@naver.com>
2024-05-22 02:52:58 +0900

    crypto-demo

--- demo-node/crypto-demo.js
+++ demo-node/crypto-demo.js
@@ -9,3 +9,7 @@ const hashPassword = crypto
   .toString('base64');
 
 console.log(hashPassword);
+
+// 회원가입 시 비밀번호를 암호화해서 암호화된 비밀번호와, salt 값을 같이 저장
+// 로그인 시, 이메일 & 비밀번호(날 것) => salt값 꺼내서 비밀번호 암호화해보고
+// => 디비 비밀번호랑 비교
