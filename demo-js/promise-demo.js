84a68c0a89bcfef17518854fbf83fe8b726d1512
Bhangdongho <bhang97@naver.com>
2024-05-30 16:00:24 +0900

    promise chaining

--- demo-js/promise-demo.js
+++ demo-js/promise-demo.js
@@ -1,6 +1,7 @@
 //  Promise "객체" : 무조건 약속을 지키는 친구
 
 let promise = new Promise(function (resolve, reject) {
+  // 매개변수로 함수를 받아요.
   // executor : 이 친구가 할 일
 
   setTimeout(() => resolve('완료!'), 3000);
@@ -9,4 +10,11 @@ let promise = new Promise(function (resolve, reject) {
   // 할 일을 성공적으로 하면 resolve(결과)
   //       실패하면 reject(에러)
 }); // 친구 소환
-// 매개변수로 함수를 받아요.
+
+// promise의 기본 메소드 : promise가 일 다 하고 (=약속 다 지키고) 호출해야하는 함수
+promise.then(
+  function (result) {
+    console.log(result);
+  },
+  function (error) {}
+);
